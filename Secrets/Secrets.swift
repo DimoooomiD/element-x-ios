293be3167ae4be5ea670db7e72cbc4d2a6c1aa35
Omid <ochaghaneh@gmail.com>
2025-11-06 22:33:49 +0100

    Disable PostHog analytics to fix connection refused errors

--- Secrets/Secrets.swift
+++ Secrets/Secrets.swift
@@ -1,7 +1,7 @@
 enum Secrets {
     static let sentryDSN: String? = "https://username@sentry.localhost/project_id"
     static let sentryRustDSN: String? = "https://username@sentry.localhost/project_id"
-    static let postHogHost: String? = "https://posthog.localhost"
+    static let postHogHost: String? = nil
     static let postHogAPIKey: String? = "your_key"
     static let rageshakeURL: String? = "https://rageshake.localhost/submit"
     static let mapLibreAPIKey: String? = "your_key"
